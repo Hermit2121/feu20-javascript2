3f9eb4103d75c34aa39ed97293a4695d40d768d2
Joakim Wahlström <wahlstrom.joakim@gmail.com>
2021-02-02 13:39:58 +0100

    lektion1

--- Lektion-1/3-core-modules/fs.js
+++ Lektion-1/3-core-modules/fs.js
@@ -31,9 +31,67 @@ const fs = require('fs');
 
 
 // DÖPA OM EN FIL
-fs.rename('./mapp/text3.txt', './mapp/text2.txt', err => {
-  if(err)
-    console.log(err);
+// fs.rename('./mapp/text3.txt', './mapp/text2.txt', err => {
+//   if(err)
+//     console.log(err);
 
-  console.log('Bytte namn på filen');
-})
+//   console.log('Bytte namn på filen');
+// })
+
+
+// MAPPAR
+// if(!fs.existsSync('./NyMapp')) {
+//   fs.mkdir('./NyMapp', err => {
+//     if (err)
+//       console.log(err);
+
+//     console.log('mapp skapad');
+//   })
+// } else {
+//   fs.rmdir('./NyMapp', err => {
+//     if (err)
+//       console.log(err);
+
+//     console.log('Mappen fanns redan så jag tar bort den..');
+//   })
+// }
+
+
+// TA BORT FILER
+
+// if(fs.existsSync('./mapp/text2.txt')) {
+
+//   fs.unlink('./mapp/text2.txt', err => {
+//     if (err)
+//       console.log(err);
+      
+//     console.log('Tar bort filen');  
+//   })
+
+// }
+
+const path = require('path');
+
+// console.log(__dirname);
+// fs.appendFile(path.join(__dirname, 'mapp', 'text.txt'), ' Nu lägger vi till text.... igen...', () => {
+//   console.log('la till text.. igen..')
+// })
+
+
+// LÄSA / SKRIVA STORA FILER / STREAMS
+
+const readStream = fs.createReadStream(path.join(__dirname, 'mapp', 'stor.txt'), { encoding: 'utf8' });
+const writeStream = fs.createWriteStream(path.join(__dirname, 'mapp', 'stor2.txt'));
+
+
+// readStream.on('data', buffer => {
+//   // console.log('-------------------------------------------------------- NY DATA ------------------------------------------------------');
+//   // console.log(buffer.toString());
+//   // console.log(buffer);
+
+//   writeStream.write('\n ----------------------------------------------------- NY DATA ------------------------------------------------------ \n');
+//   writeStream.write(buffer);
+//   console.log('skrev en blit till filen');
+// })
+
+// readStream.pipe(writeStream);
