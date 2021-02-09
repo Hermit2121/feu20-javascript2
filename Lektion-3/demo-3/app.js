2a580abb25582d22a6e8fbf5481aa8b186815015
Joakim Wahlström <wahlstrom.joakim@gmail.com>
2021-02-09 11:52:28 +0100

    lektion-3

--- Lektion-3/demo-3/app.js
+++ Lektion-3/demo-3/app.js
@@ -1,6 +1,12 @@
 const express = require('express');
 const app = express();
 
+// importera controllers
+const todosController = require('./controllers/todosController');
 
+
+
+// Controllers
+app.use('/api/todos', todosController);
 
 module.exports = app;
--- Lektion-3/demo-3/server.js
+++ Lektion-3/demo-3/server.js
@@ -3,6 +3,14 @@ const mongoose = require('mongoose');
 const port = 9999;
 
 const serverURI = 'http://localhost:' + port;
+const mongoURI = 'mongodb+srv://Joakim:BytMig123@cluster0.o5bxd.mongodb.net/Todos?retryWrites=true&w=majority'
 
 
 app.listen(port, () => console.log('Wabserver: ' + serverURI));
+
+mongoose
+.set('useCreateIndex', true)
+.connect(mongoURI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+}, () => console.log('Connected to DB'));
