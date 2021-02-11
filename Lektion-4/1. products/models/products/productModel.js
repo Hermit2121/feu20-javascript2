53c41c2a231dc5ef9d6a9b86961be412bcb3139f
Joakim Wahlström <wahlstrom.joakim@gmail.com>
2021-02-11 11:51:50 +0100

    lektion-4

--- Lektion-4/1. products/models/products/productModel.js
+++ Lektion-4/1. products/models/products/productModel.js
@@ -19,8 +19,8 @@ exports.createProduct = (req, res) => {
 
   Product.find({ name: req.body.name })
   .then(data => {
-
-    if(data > 0) {
+    // console.log(data)
+    if(data.length > 0) {
       return res.status(400).json({
         statusCode: 400,
         status: false,
@@ -38,7 +38,66 @@ exports.createProduct = (req, res) => {
 
     })
 
-
-
+    product.save()
+      .then(() => {
+        res.status(201).json({
+          statusCode: 201,
+          status: true,
+          message: 'Product created'
+        })
+      })
+      .catch(() => {
+        res.status(500).json({
+          statusCode: 500,
+          status: false,
+          message: 'Failed to create product'
+        })
+      })
   })
+}
+
+
+exports.updateProduct = (req, res) => {
+  // Product.updateOne( { _id: req.params.id }, req.body )
+
+  //   .then(() => {
+  //     Product.updateOne( { _id: req.params.id }, {
+  //       $set: { modified: Date.now() }
+  //     })
+
+  //     .then(() => {
+  //       res.status(200).json({
+  //         statusCode: 200,
+  //         status: true,
+  //         message: 'Product updated'
+  //       })
+  //     })
+  //   })
+  //   .catch(() => {
+  //     res.status(500).json({
+  //       statusCode: 500,
+  //       status: false,
+  //       message: 'Failed to update product'
+  //     })
+  //   })
+
+  Product.updateOne( { _id: req.params.id }, {
+    ...req.body,
+    modified: Date.now()
+  })
+  .then(() => {
+    res.status(200).json({
+      statusCode: 200,
+      status: true,
+      message: 'Product updated'
+    })
+  })
+  .catch(() => {
+    res.status(500).json({
+      statusCode: 500,
+      status: false,
+      message: 'Failed to update product'
+    })
+  })
+
 }
