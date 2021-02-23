de9036f45a194af6cf68b9ad1bcc7470ab7a741c
Joakim Wahlström <wahlstrom.joakim@gmail.com>
2021-02-23 11:51:01 +0100

    lektion 7

--- Lektion-7/2.functions/src/App.vue
+++ Lektion-7/2.functions/src/App.vue
@@ -1,17 +1,41 @@
 <template>
   <div>
     <app-nav />
+    <div class="output container mt-5">
+      <h2>{{ message }}</h2>
+
+      <functions class="mt-5" />
+      <computed class="mt-5" />
+
+
+    </div>
+
+    <app-footer msg="meddelande" />
+    <app-footer v-bind:msg="message" />
+    <app-footer :msg="message" />
   </div>
 </template>
 
 <script>
 import appNav from './components/Navbar'
+import appFooter from './components/Footer'
+import Functions from './components/Functions.vue'
+import Computed from './components/Computed.vue'
 export default {
   name: 'App',
+  data() {
+    return {
+      message: 'Ett meddelande från våran data'
+    }
+  },
   components: {
-    appNav
+    appNav,
+    appFooter,
+    Functions,
+    Computed
   }
 }
+
 </script>
 
 <style>
@@ -23,5 +47,11 @@ export default {
 .container {
   width: 80%;
   margin: auto;
+}
+.output {
+  min-height: 500px;
+}
+.mt-5 {
+  margin-top: 3rem;
 }
 </style>
---
+++ Lektion-7/2.functions/src/components/Functions.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <!-- <button @click="sum++" >Add</button>
+    <button @click="sum--">Subtract</button> -->
+    <button @click="add($event, 2)" >Add</button>
+    <button @click="sub">Subtract</button>
+
+    <div>sum: {{ sum }}</div>
+  </div>
+</template>
+
+<script>
+
+export default {
+  data() {
+    return {
+      sum: 32
+    }
+  },
+  methods: {
+    add(e, n) {
+      console.log(e)
+      this.sum += n
+    },
+    sub() {
+      this.sum--
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
