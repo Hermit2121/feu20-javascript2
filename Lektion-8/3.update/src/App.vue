dbd1bc9f747698de7597a15eedb68dd803b7788f
Joakim Wahlström <wahlstrom.joakim@gmail.com>
2021-02-25 11:46:31 +0100

    lektion 8

---
+++ Lektion-8/3.update/src/App.vue
@@ -0,0 +1,54 @@
+<template>
+  <div class="container mt-5">
+    <div class="card-deck">
+      <display :name="name" :age="age" :email="email" />
+      <update :name="name" :age="age" :email="email" @update-user="update" />
+    </div>
+    <div class="card-deck mt-5">
+      <display2 :user="user" />
+      <update2 :user="user" @update-user="update" />
+    </div>
+  </div>
+</template>
+
+<script>
+import Display from './components/Display.vue'
+import Update from './components/Update'
+import Display2 from './components/Display2.vue'
+import Update2 from './components/Update2'
+
+
+export default {
+  name: 'App',
+  components: {
+    Display,
+    Update,
+    Display2,
+    Update2
+
+  },
+  data() {
+    return {
+      name: 'Joakim',
+      age: '34',
+      email: 'joakim.wahlstrom@mail.com',
+      user: {
+        name: 'Joakim',
+        age: '34',
+        email: 'joakim.wahlstrom@mail.com'
+      }
+    }
+  },
+  methods: {
+    update(newUser) {
+      this.name = newUser.name,
+      this.age = newUser.age,
+      this.email = newUser.email
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ Lektion-8/3.update/src/components/Update2.vue
@@ -0,0 +1,70 @@
+<template>
+<!-- Material form register -->
+<div class="card">
+
+    <h5 class="card-header blue-gradient white-text text-center py-4">
+        <strong>Sign up</strong>
+    </h5>
+
+    <!--Card content-->
+    <div class="card-body px-lg-5 pt-0">
+
+        <!-- Form -->
+        <form class="text-center" style="color: #757575;" action="#!" @submit.prevent="update">
+
+            <div class="form-row">
+                <div class="col">
+                    <!-- First name -->
+                    <div class="md-form">
+                        <input type="text" id="name" class="form-control" v-model="newName">
+                        <label for="name">Name</label>
+                    </div>
+                </div>
+                <div class="col">
+                    <!-- Age -->
+                    <div class="md-form">
+                        <input type="number" id="age" class="form-control" v-model="newAge">
+                        <label for="age">Age</label>
+                    </div>
+                </div>
+            </div>
+
+            <!-- E-mail -->
+            <div class="md-form mt-0">
+                <input type="email" id="email" class="form-control" v-model="newEmail">
+                <label for="email">E-mail</label>
+            </div>
+
+            <!-- Sign up button -->
+            <button class="btn btn-outline-info btn-rounded btn-block my-4 waves-effect z-depth-0" type="submit">Sign in</button>
+
+        </form>
+        <!-- Form -->
+    </div>
+</div>
+<!-- Material form register -->
+</template>
+
+<script>
+export default {
+  props: ['user'],
+  data() {
+    return {
+      newName: this.user.name,
+      newAge: this.user.age,
+      newEmail: this.user.email
+    }
+  },
+  methods: {
+    update() {
+      this.user.name = this.newName
+      this.user.age = this.newAge
+      this.user.email = this.newEmail
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
