a081a5504ef66c93827cee562f821194a3a99fd2
Joakim Wahlström <wahlstrom.joakim@gmail.com>
2021-03-11 10:10:44 +0100

    lektion 10

---
+++ Lektion-10/1.blog/src/App.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="m-3 mt-5">
+    <button class="btn btn-primary" @click="component = 'CreatePost'">Create</button>
+    <button class="btn btn-primary" @click="component = 'ViewPosts'">View</button>
+    <div class="border border-primary">
+      <div class="container">
+        <component :is="component" />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import CreatePost from './components/CreatePost'
+import ViewPosts from './components/ViewPosts'
+
+export default {
+  name: 'App',
+  data() {
+    return {
+      posts: [],
+      component: 'CreatePost'
+    }
+  },
+  components: {
+    CreatePost,
+    ViewPosts
+  }
+
+}
+</script>
+
+<style>
+
+</style>
