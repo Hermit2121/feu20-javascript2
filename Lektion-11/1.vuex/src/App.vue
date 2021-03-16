e2b459c9246e006cda5f835ccc4c5e25ff1fe718
Joakim Wahlström <wahlstrom.joakim@gmail.com>
2021-03-16 09:45:38 +0100

    lektion 11

---
+++ Lektion-11/1.vuex/src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <navbar />
+    <router-view/>
+  </div>
+</template>
+
+<script>
+import Navbar from './components/Navbar'
+export default {
+  components: { Navbar }
+}
+</script>
+
+<style>
+* {
+  box-sizing: border-box;
+  margin: 0;
+  padding: 0;
+}
+
+</style>
---
+++ Lektion-11/1.vuex/src/views/Products.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="products">
+    <aside>
+      <div class="buttons">
+        <button><i class="fas fa-th-large"></i></button>
+        <button><i class="fas fa-list"></i></button>
+      </div>
+      <form>
+        <div>
+          <input type="text" placeholder="search...">
+        </div>
+      </form>
+      <div>
+        <a href="#">Filter</a>
+      </div>
+    </aside>
+    <div class="content">
+      <component :is="comp" />
+    </div>
+  </div>
+</template>
+
+<script>
+import Grid from '../components/Products/Grid'
+import List from '../components/Products/List'
+export default {
+  name: 'Products',
+  components: {
+    Grid,
+    List
+  },
+  data() {
+    return {
+      comp: 'Grid'
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
