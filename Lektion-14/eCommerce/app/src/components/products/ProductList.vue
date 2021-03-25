e7f87d594acba061743c6094ebdf93f5f924aa16
Joakim Wahlström <wahlstrom.joakim@gmail.com>
2021-03-25 10:11:15 +0100

    lektion 14

---
+++ Lektion-14/eCommerce/app/src/components/products/ProductList.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="row row-cols-1 row-cols-md-3">
+    <product v-for="product in products" :key="product._id" />
+  </div>
+</template>
+
+<script>
+import Product from './Product'
+export default {
+  components: {
+    Product
+  }
+}
+</script>
+
+<style>
+
+</style>
